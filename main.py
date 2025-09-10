903240658eae7898a44a4c47110f54aff9037cf7
Denys Talko <sf1vs1@gmail.com>
2025-09-10 16:27:53 +0300

    add users and install pydantic[email]

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 import uvicorn
+
+from users.views import router as users_router
 from core.init_db import init_db
 
 from app import views
@@ -17,7 +19,7 @@ app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(views.router)
-
+app.include_router(users_router)
 
 @app.get("/")
 async def root():
